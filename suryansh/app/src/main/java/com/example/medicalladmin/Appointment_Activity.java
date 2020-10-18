f2444436ac4d200f7936b3ab44d1c64b318ae370
suryansh bisen <suryansh.1191@gmail.com>
2020-10-18 12:22:15 +0530

    suryansh123

---
+++ suryansh/app/src/main/java/com/example/medicalladmin/Appointment_Activity.java
@@ -0,0 +1,14 @@
+package com.example.medicalladmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Appointment_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_);
+    }
+}
---
+++ suryansh/app/src/main/java/com/example/medicalladmin/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.medicalladmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button logout;
+    private Button barcode;
+    private Button Appointment;
+    private FirebaseAuth mAuth;
+    private Button docter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mAuth = FirebaseAuth.getInstance();
+        Appointment = findViewById(R.id.Apponment);
+        barcode = findViewById(R.id.barcode);
+        logout = findViewById(R.id.logOut);
+        docter = findViewById(R.id.doctersList);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this , login_Activity.class));
+                Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Appointment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , Appointment_Activity.class));
+            }
+        });
+        barcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , barcode_Activity2.class));
+            }
+        });
+        docter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , Docter_activity.class));
+            }
+        });
+
+
+
+    }
+}
